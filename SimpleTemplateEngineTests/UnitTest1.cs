9c91c759b7c6cd3c5a41835da1b02c9c94634481
Andres Moschini <andresmoschini@gmail.com>
2014-11-05 14:30:29 -0300

    Create solution and projects

---
+++ SimpleTemplateEngineTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleTemplateEngineTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
