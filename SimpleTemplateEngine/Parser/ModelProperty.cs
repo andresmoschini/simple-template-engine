2701d20c5b2eb3c1ff95bdbd7658e6a0bea876d8
Andres Moschini <andresmoschini@gmail.com>
2014-11-06 17:08:46 -0300

    Add ModelProperty class

---
+++ SimpleTemplateEngine/Parser/ModelProperty.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTemplateEngine.Parser
+{
+    public class ModelProperty
+    {
+        public bool BooleanValue { get; set; }
+        public string StringValue { get; set; }
+        public Object[] ListValue { get; set; }
+
+        public static ModelProperty FromObject(object model, string propertyName)
+        {
+            var property = model.GetType().GetProperty(propertyName);
+            var type = property.PropertyType;
+            var value = property.GetGetMethod().Invoke(model, null);
+
+            if (value == null)
+            {
+                return new ModelProperty()
+                {
+                    BooleanValue = false,
+                    StringValue = "[Empty]",
+                    ListValue = new object[] { }
+                };
+            }
+            else if (type == typeof(string))
+            {
+                var str = (string)value;
+                return new ModelProperty()
+                {
+                    BooleanValue = str.Length > 0,
+                    StringValue = str,
+                    ListValue = new object[] { }
+                };
+            }
+            else if (type == typeof(bool))
+            {
+                return new ModelProperty()
+                {
+                    BooleanValue = (bool)value,
+                    StringValue = value.ToString(),
+                    ListValue = new object[] { }
+                };
+            }
+            else if (type.GetInterfaces().Contains(typeof(IEnumerable)))
+            {
+                var list = ((IEnumerable)value).Cast<object>().ToArray();
+                var count = list.Count();
+                return new ModelProperty()
+                {
+                    BooleanValue = count > 0,
+                    StringValue = count == 0 ? "[Empty]" : (count == 1 ? "[1 item]" : string.Format("[{0} items]", count)),
+                    ListValue = list
+                };
+            }
+            else
+            {
+                var str = value.ToString();
+                return new ModelProperty()
+                {
+                    BooleanValue = str.Length > 0,
+                    StringValue = str,
+                    ListValue = new object[] { }
+                };
+            }
+        }
+    }
+}
---
+++ SimpleTemplateEngineTests/ModelPropertyTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTemplateEngine;
+using SimpleTemplateEngine.Parser;
+using System.Collections.Generic;
+
+namespace SimpleTemplateEngineTests
+{
+    [TestClass]
+    public class ModelPropertyTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var o = new
+            {
+                StringProperty = "StringValue",
+                EmptyStringProperty = "",
+                BooleanPropertyTrue = true,
+                BooleanPropertyFalse = false,
+                EmptyListProperty = new object[] { },
+                NullListProperty = (List<string>)null,
+                StringListProperty = new[] { "item1", "item2" },
+                SingleItemStringListProperty = new[] { "item1" },
+                ObjectListProperty = new[] { new { name = "value1" }, new { name = "value2" } }
+            };
+
+            var stringProperty = ModelProperty.FromObject(o, "StringProperty");
+            Assert.IsTrue(stringProperty.BooleanValue);
+            Assert.AreEqual("StringValue", stringProperty.StringValue);
+            Assert.AreEqual(0, stringProperty.ListValue.Length);
+
+            var emptyStringProperty = ModelProperty.FromObject(o, "EmptyStringProperty");
+            Assert.IsFalse(emptyStringProperty.BooleanValue);
+            Assert.AreEqual("", emptyStringProperty.StringValue);
+            Assert.AreEqual(0, emptyStringProperty.ListValue.Length);
+
+            var booleanPropertyTrue = ModelProperty.FromObject(o, "BooleanPropertyTrue");
+            Assert.IsTrue(booleanPropertyTrue.BooleanValue);
+            Assert.AreEqual("True", booleanPropertyTrue.StringValue);
+            Assert.AreEqual(0, booleanPropertyTrue.ListValue.Length);
+
+            var booleanPropertyFalse = ModelProperty.FromObject(o, "BooleanPropertyFalse");
+            Assert.IsFalse(booleanPropertyFalse.BooleanValue);
+            Assert.AreEqual("False", booleanPropertyFalse.StringValue);
+            Assert.AreEqual(0, booleanPropertyFalse.ListValue.Length);
+
+            var emptyListProperty = ModelProperty.FromObject(o, "EmptyListProperty");
+            Assert.IsFalse(emptyListProperty.BooleanValue);
+            Assert.AreEqual("[Empty]", emptyListProperty.StringValue);
+            Assert.AreEqual(0, emptyListProperty.ListValue.Length);
+
+            var nullListProperty = ModelProperty.FromObject(o, "NullListProperty");
+            Assert.IsFalse(nullListProperty.BooleanValue);
+            Assert.AreEqual("[Empty]", nullListProperty.StringValue);
+            Assert.AreEqual(0, nullListProperty.ListValue.Length);
+
+            var stringListProperty = ModelProperty.FromObject(o, "StringListProperty");
+            Assert.IsTrue(stringListProperty.BooleanValue);
+            Assert.AreEqual("[2 items]", stringListProperty.StringValue);
+            Assert.AreEqual(2, stringListProperty.ListValue.Length);
+
+            var singleItemStringListProperty = ModelProperty.FromObject(o, "SingleItemStringListProperty");
+            Assert.IsTrue(singleItemStringListProperty.BooleanValue);
+            Assert.AreEqual("[1 item]", singleItemStringListProperty.StringValue);
+            Assert.AreEqual(1, singleItemStringListProperty.ListValue.Length);
+
+            var objectListProperty = ModelProperty.FromObject(o, "ObjectListProperty");
+            Assert.IsTrue(objectListProperty.BooleanValue);
+            Assert.AreEqual("[2 items]", objectListProperty.StringValue);
+            Assert.AreEqual(2, objectListProperty.ListValue.Length);
+        }
+    }
+}
